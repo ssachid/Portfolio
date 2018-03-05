b731ee36e99b41bf72a933c276215ce1647214c2
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-05 10:47:42 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: Faker::Lorem.sentence
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |profile_item|
+  Profile.create!(
+    title: "Profile #{profile_item}",
+    subtitle: "My great service",
+    body: Faker::Lorem.sentence,
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
