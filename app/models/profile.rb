cb431a3ca9ee4e7023454bbb35f25c98bae888d3
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-06 08:10:08 -0800

    Implemented concern for managing image data

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,12 @@
 class Profile < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   scope :ruby_on_rails_profile_items, -> { where(subtitle: 'Ruby on Rails')}
   after_initialize :set_defaults
 
   private
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/200x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '250')
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :percent_utilized
+  after_initialize :set_defaults
+
+  private
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '350', width: '250')
+  end
 end
