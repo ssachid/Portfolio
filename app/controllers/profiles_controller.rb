b28ec0d43da8715a2b83f6846245be39009f4e13
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-06 11:26:56 -0800

    Integrated new and create actions to Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,24 @@ class ProfilesController < ApplicationController
   def index
     @profile_items = Profile.all
   end
+
+  def new
+    @profile_item = Profile.new
+  end
+
+  def create
+    @profile_item = Profile.new(profile_params)
+    respond_to do |format|
+      if @profile_item.save
+        format.html { redirect_to profiles_path, notice: "Your profile is now live."}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:title, :subtitle, :body)
+  end
 end
