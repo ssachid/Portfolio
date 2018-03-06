9ad071b909440e989f2364854138ff3617283a21
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-06 07:45:41 -0800

    Implemented image defaults for profile items

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,11 @@
 class Profile < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
   scope :ruby_on_rails_profile_items, -> { where(subtitle: 'Ruby on Rails')}
+  after_initialize :set_defaults
+
+  private
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/200x200"
+  end
 end
