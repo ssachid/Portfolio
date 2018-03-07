c4f8a4a2d1580f48f392c9d3a97e8903ce517770
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-07 09:37:29 -0800

    Integrated nested attributes for profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
-  include Placeholder
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs[:name].blank? }
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   scope :ruby_on_rails_profile_items, -> { where(subtitle: 'Ruby on Rails')}
   after_initialize :set_defaults
