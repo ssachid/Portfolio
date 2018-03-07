7c83fb8c4086ba5a99d8fa356c19bd28fad23f10
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-06 18:16:38 -0800

    Implemented custom action for updating status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,10 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.order(:created_at)
   end
 
   # GET /blogs/1
@@ -59,6 +59,15 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.published?
+      @blog.draft!
+    elsif @blog.draft?
+      @blog.published!
+    end
+    redirect_to blogs_url, notice: "Blog status has been updated"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :blogs
   resources :profiles, except: [:show]
   get '/profile/:id', to: "profiles#show", as: 'profile_show'
   get 'pages/home'
   root "pages#home"
   get "about-me", to: 'pages#about'
   get "contact", to: 'pages#contact'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
